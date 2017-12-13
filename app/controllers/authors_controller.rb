d0d0e1800b38d322b2b910b9e376b0129e4a42d8
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-13 23:22:32 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,11 +7,10 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-    if @author.errors.any?
-      render :new
-    else
+    if Author.create!(author_params)
       redirect_to author_path(@author)
+    else
+      render :new
     end
   end
 
